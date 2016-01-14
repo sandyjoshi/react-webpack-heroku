b821bb4a8988a36abed225594f66e9ac80e5ce28
sandeep joshi <joshi.sandeep.d@gmail.com>
2016-01-14 18:27:42 +0530

    remove extra component grid

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import AppBar from 'material-ui/lib/app-bar';
 import FlatButton from 'material-ui/lib/flat-button';
-import Grid from './grid';
+// import Grid from './grid';
 import IconButton from 'material-ui/lib/icon-button';
 import {connect} from 'react-redux';
 import { bindActionCreators } from 'redux';
 import dialogActions from '../actions/dialog';
 import AddDialog from './addDialog';
 import DeleteDialog from './deleteDialog';
+import ContactItem from './contactItem';
+
 
 const mapStateToProps = (state) => ({
     contacts: state.dialog.contacts,
@@ -23,23 +25,16 @@ const mapDispatchToProps = (dispatch) => ({
 export default class ContactManagerView extends React.Component {
   constructor() {
     super();
-    // ToDo : bind methods here , propType entries
+    this.handleShowDeleteDialog = this.handleShowDeleteDialog.bind(this);
+    this.handleShowAddDialog = this.handleShowAddDialog.bind(this);
+    // ToDo : propType entries
   };
 
-  handleAdd(){
-    this.props.dialogActions.showAddDialog();
-    // this.props.history.pushState(null, '/add/');
-  };
-
-  // handleCloseDialog = () => {
-  //   this.props.dialogActions.closeDeleteDialog();
+  // handleAdd(){
+  //   this.props.dialogActions.showAddDialog();
   // };
 
-  // handleDeleteConfirmed = () => {
-  //   this.props.dialogActions.deleteConfirmed();
-  // };
-
-  handleShowDialog = (id) => {
+  handleShowDeleteDialog = (id) => {
     this.props.dialogActions.showDeleteDialog(id);
   };
 
@@ -47,14 +42,24 @@ export default class ContactManagerView extends React.Component {
     this.props.dialogActions.showAddDialog(id);
   };
 
-  render() {
+  generateContactItems = () => {
+    return (
+      <div className='grid'>
+        {this.props.contacts.map(function (item, index) {
+            return ( <ContactItem {...item} onDeleteDialog={this.handleShowDeleteDialog} onEditDialog={this.handleShowAddDialog}/> )
+          }, this)
+        }
+      </div>
+    );
+  }
 
+  render() {
     return(
       <div>
-        <AppBar title="Contact Manager" onRightIconButtonTouchTap={this.handleAdd.bind(this)} showMenuIconButton={false} iconClassNameRight="icon-add" />
+        <AppBar title="Contact Manager" onRightIconButtonTouchTap={this.handleShowAddDialog.bind(this)} showMenuIconButton={false} iconClassNameRight="icon-add" />
         <div className="grid-container">
           <h1 className="header"> List of Contacts </h1>
-          <Grid contacts={this.props.contacts} onDeleteDialog={this.handleShowDialog.bind(this)} onEditDialog={this.handleShowAddDialog.bind(this)} />
+          {this.generateContactItems()}
           <DeleteDialog deleteDialog={this.props.deleteDialog} dialogActions={this.props.dialogActions} />
           <AddDialog addDialog={this.props.addDialog} dialogActions={this.props.dialogActions} editingContact={this.props.editingContact} />
         </div>
