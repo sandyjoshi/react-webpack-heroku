bfdbae5c6188bab5dc6712339e3457a8f35e620c
sandeep joshi <joshi.sandeep.d@gmail.com>
2016-01-14 19:39:52 +0530

    restructured css & contactItem component

--- src/components/contactItem.jsx
+++ src/components/contactItem.jsx
@@ -3,8 +3,17 @@ import RaisedButton from 'material-ui/lib/raised-button';
 import FontIcon from 'material-ui/lib/font-icon';
 import IconButton from 'material-ui/lib/icon-button';
 
+const styleDeleteButton = { padding : 0 , height : '32px' , width : '32px' };
+const styleEditButton = { padding : 0 , position : 'absolute', bottom : '0', height : '26px', minWidth : '52px' };
 
 export default class Item extends React.Component {
+  static propTypes = {
+    id: React.PropTypes.number.isRequired,
+    name: React.PropTypes.string.isRequired,
+    email: React.PropTypes.string.isRequired,
+    tel: React.PropTypes.string.isRequired
+  };
+
   constructor() {
     super();
     this.handleCloseClick = this.handleCloseClick.bind(this);
@@ -20,12 +29,10 @@ export default class Item extends React.Component {
   };
 
   render () {
-    let style = { padding : 0 , height : '32px' , width : '32px' };
-    let style2 = { padding : 0 , position : 'absolute', bottom : '0', height : '26px', minWidth : '52px' };
     // ToDo : Change icon button. there is no click event.
     return (
       <div className='contact-item'>
-        <IconButton className="delete-button" style={style} onFocus={this.handleCloseClick} iconClassName="icon-delete" />
+        <IconButton className="delete-button" style={styleDeleteButton} onFocus={this.handleCloseClick} iconClassName="icon-delete" />
         <div className="image"></div>
         <div className="details">
           <h3 className="name" title={ this.props.name }>{ this.props.name }</h3>
@@ -33,7 +40,7 @@ export default class Item extends React.Component {
           <div className="value" title={ this.props.tel }>{ this.props.tel }</div>
           <div className="title">Email :</div>
           <div className="value" title={ this.props.email }>{ this.props.email }</div>
-          <RaisedButton style={style2} label="edit" onTouchTap={this.handleEditClick}/>
+          <RaisedButton style={styleEditButton} label="edit" onTouchTap={this.handleEditClick}/>
         </div>
       </div>
     );
