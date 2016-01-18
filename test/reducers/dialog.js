725da2d54eafbfae2d7bca221ea8dee63fa3c531
sandeep joshi <joshi.sandeep.d@gmail.com>
2016-01-18 10:54:33 +0530

    organized test cases

--- test/reducers/dialog.js
+++ test/reducers/dialog.js
@@ -2,7 +2,7 @@ import { expect, assert } from 'chai';
 import reducer from '../../src/reducers/dialog'
 import * as types from '../../src/constants/dialog'
 
-describe('Delete contact dialog reducer', () => {
+describe('Dialog reducer', () => {
   it('should return the initial state', () => {
     let state = reducer(undefined, {});
     assert.isObject(state);
@@ -13,67 +13,73 @@ describe('Delete contact dialog reducer', () => {
     assert.isArray( state.contacts );
   })
 
-  it('should handle CLOSE_DELETE_DIALOG', () => {
-    let state = reducer({}, { type: types.CLOSE_DELETE_DIALOG });
-    assert.isObject(state);
-    expect( state.deleteDialog ).to.eql( false );
-    assert.isNull( state.deletingContactId );
+  describe('Delete contact dialog reducer', () => {
+
+    it('should handle CLOSE_DELETE_DIALOG', () => {
+      let state = reducer({}, { type: types.CLOSE_DELETE_DIALOG });
+      assert.isObject(state);
+      expect( state.deleteDialog ).to.eql( false );
+      assert.isNull( state.deletingContactId );
+    })
+
+    it('should handle SHOW_DELETE_DIALOG', () => {
+      const contactId = 1 ;
+      let state = reducer({}, { type: types.SHOW_DELETE_DIALOG , payload : contactId });
+      assert.isObject(state);
+      expect( state.deleteDialog ).to.eql( true );
+      expect( state.deletingContactId ).to.eql( contactId );
+    })
+
+    it('should handle DELETE_CONFIRMED', () => {
+      const contactId = 1 ;
+      let state = reducer({ deletingContactId : contactId , contacts : [ { id : contactId } ] }, { type: types.DELETE_CONFIRMED });
+      assert.isObject(state);
+      expect( state.deleteDialog ).to.eql( false );
+      assert.isArray( state.contacts );
+      expect( state.contacts.length ).to.eql( 0 );
+    })
   })
 
-  it('should handle SHOW_DELETE_DIALOG', () => {
-    const contactId = 1 ;
-    let state = reducer({}, { type: types.SHOW_DELETE_DIALOG , payload : contactId });
-    assert.isObject(state);
-    expect( state.deleteDialog ).to.eql( true );
-    expect( state.deletingContactId ).to.eql( contactId );
-  })
+  describe('Add/Update contact dialog reducer', () => {
 
-  it('should handle DELETE_CONFIRMED', () => {
-    const contactId = 1 ;
-    let state = reducer({ deletingContactId : contactId , contacts : [ { id : contactId } ] }, { type: types.DELETE_CONFIRMED });
-    assert.isObject(state);
-    expect( state.deleteDialog ).to.eql( false );
-    assert.isArray( state.contacts );
-    expect( state.contacts.length ).to.eql( 0 );
-  })
+    it('should handle SHOW_ADD_DIALOG for adding contact', () => {
+      let state = reducer({ contacts : [] }, { type: types.SHOW_ADD_DIALOG });
+      assert.isObject(state);
+      expect( state.addDialog ).to.eql( true );
+      expect( state.editingContact ).to.be.undefined;
+    })
 
-  it('should handle SHOW_ADD_DIALOG for adding contact', () => {
-    let state = reducer({ contacts : [] }, { type: types.SHOW_ADD_DIALOG });
-    assert.isObject(state);
-    expect( state.addDialog ).to.eql( true );
-    expect( state.editingContact ).to.be.undefined;
-  })
+    it('should handle SHOW_ADD_DIALOG for updating contact', () => {
+      const contactId = 1 ;
+      let state = reducer({ contacts : [ { id : contactId } ] }, { type: types.SHOW_ADD_DIALOG , payload : contactId });
+      assert.isObject(state);
+      expect( state.addDialog ).to.eql( true );
+      expect( state.editingContact.id ).to.eql( contactId );
+    })
 
-  it('should handle SHOW_ADD_DIALOG for updating contact', () => {
-    const contactId = 1 ;
-    let state = reducer({ contacts : [ { id : contactId } ] }, { type: types.SHOW_ADD_DIALOG , payload : contactId });
-    assert.isObject(state);
-    expect( state.addDialog ).to.eql( true );
-    expect( state.editingContact.id ).to.eql( contactId );
-  })
+    it('should handle CLOSE_ADD_DIALOG', () => {
+      let state = reducer({}, { type: types.CLOSE_ADD_DIALOG });
+      assert.isObject(state);
+      expect( state.addDialog ).to.eql( false );
+      assert.isNull( state.editingContact );
+    })
 
-  it('should handle CLOSE_ADD_DIALOG', () => {
-    let state = reducer({}, { type: types.CLOSE_ADD_DIALOG });
-    assert.isObject(state);
-    expect( state.addDialog ).to.eql( false );
-    assert.isNull( state.editingContact );
-  })
+    it('should handle ADD_UPDATE_CONTACT for adding contact', () => {
+      const contact = { name : 'abc' } ;
+      let state = reducer({ contacts : [] }, { type: types.ADD_UPDATE_CONTACT , payload : contact });
+      assert.isObject(state);
+      assert.isArray( state.contacts );
+      expect( state.contacts.length ).to.eql( 1 );
+    })
 
-  it('should handle ADD_CONTACT', () => {
-    const contact = { name : 'abc' } ;
-    let state = reducer({ contacts : [] }, { type: types.ADD_UPDATE_CONTACT , payload : contact });
-    assert.isObject(state);
-    assert.isArray( state.contacts );
-    expect( state.contacts.length ).to.eql( 1 );
-  })
-
-  it('should handle UPDATE_CONTACT', () => {
-    const contact = { name : 'abc' , id : 2 } ;
-    let state = reducer({ contacts : [ { name : 'xyz' , id : 2 } ] }, { type: types.ADD_UPDATE_CONTACT , payload : contact });
-    assert.isObject(state);
-    assert.isArray( state.contacts );
-    expect( state.contacts.length ).to.eql( 1 );
-    expect( state.contacts[0].name ).to.eql( 'abc' );
+    it('should handle ADD_UPDATE_CONTACT for updating contact', () => {
+      const contact = { name : 'abc' , id : 2 } ;
+      let state = reducer({ contacts : [ { name : 'xyz' , id : 2 } ] }, { type: types.ADD_UPDATE_CONTACT , payload : contact });
+      assert.isObject(state);
+      assert.isArray( state.contacts );
+      expect( state.contacts.length ).to.eql( 1 );
+      expect( state.contacts[0].name ).to.eql( 'abc' );
+    })
   })
 
 })
